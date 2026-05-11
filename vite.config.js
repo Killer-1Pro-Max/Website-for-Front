8dc44b025fe1179c0630514f21431a9b2a2d2311
Killer-1Pro-Max <siroj.comp.2003@gmail.com>
2026-05-11 19:35:07 +0500

    build for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Website-for-Front/'
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> cc09483 (build for github pages)
